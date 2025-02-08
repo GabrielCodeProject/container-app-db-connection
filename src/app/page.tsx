3284e532d01021e50a44cea6291ff551c9060d91
Gabriel <gabrielprivermsg@gmail.com>
2025-02-08 14:15:23 -0500

    add prisma dependencie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,37 @@
 import Image from "next/image";
+import { addTask } from "./actions";
 
-export default function Home() {
+export default async function Home() {
+  const tasks = [
+    {
+      id: 1,
+      name: "task 1",
+    },
+    {
+      id: 2,
+      name: "task 2",
+    },
+  ];
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1>Tasks:</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <ul className="my-10 text-center">
+          {tasks.map((task) => (
+            <li key={task.id}>{task.name}</li>
+          ))}
+        </ul>
+
+        <form action={addTask} className="space-x-2 h-4">
+          <input type="text" name="title" className="px-3 py-1 rounded" />
+          <button
+            type="submit"
+            className="bg-blue-500 px-3 py-1 text-white rounded"
           >
-            Read our docs
-          </a>
-        </div>
+            Add task
+          </button>
+        </form>
       </main>
     </div>
   );
