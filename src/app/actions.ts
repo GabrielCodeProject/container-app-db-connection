4e516a9e4e34eb7a1ad3900bafe989fede5ff5d5
Gabriel <gabrielprivermsg@gmail.com>
2025-02-08 15:28:01 -0500

    add vps scripts build with prisma

--- src/app/actions.ts
+++
@@ -1,5 +0,0 @@
-"use server";
-
-export async function addTask(formData: FormData) {
-  console.log("adding task...");
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,44 @@
-import Image from "next/image";
-import { addTask } from "./actions";
+import { addTask } from "../actions/actions";
+import prisma from "@/lib/db";
 
 export default async function Home() {
-  const tasks = [
-    {
-      id: 1,
-      name: "task 1",
-    },
-    {
-      id: 2,
-      name: "task 2",
-    },
-  ];
+  //prisma client
+  const tasks = await prisma.task.findMany();
+  //  const tasks = [
+  //    {
+  //      id: 1,
+  //      name: "task 1",
+  //    },
+  //    {
+  //      id: 2,
+  //      name: "task 2",
+  //    },
+  //  ];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Tasks:</h1>
+    <div className="flex flex-col flex-wrap items-center bg-white min-h-screen">
+      <h1 className="text-black">Tasks:</h1>
 
-        <ul className="my-10 text-center">
-          {tasks.map((task) => (
-            <li key={task.id}>{task.name}</li>
-          ))}
-        </ul>
+      <ul className="my-10 text-center">
+        {tasks.map((task) => (
+          <li className="text-black" key={task.id}>
+            {task.title}
+          </li>
+        ))}
+      </ul>
 
-        <form action={addTask} className="space-x-2 h-4">
-          <input type="text" name="title" className="px-3 py-1 rounded" />
-          <button
-            type="submit"
-            className="bg-blue-500 px-3 py-1 text-white rounded"
-          >
-            Add task
-          </button>
-        </form>
-      </main>
+      <form action={addTask} className="space-x-2 h-4">
+        <input
+          type="text"
+          name="title"
+          className="px-3 py-1 rounded text-black border-2 border-gray-500"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 px-3 py-1 text-white rounded"
+        >
+          Add task
+        </button>
+      </form>
     </div>
   );
 }
